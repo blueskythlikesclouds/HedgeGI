af2554b911d20750ade6b055cc60edffbeb1cc44
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-08-04 15:29:48 +0300

    Implement Generations' gamma correction.

--- Source/HedgeGI/ToneMap.frag.glsl
+++ Source/HedgeGI/ToneMap.frag.glsl
@@ -6,12 +6,10 @@ in float fScale;
 out vec4 oColor;
 
 uniform sampler2D uTexture;
-uniform bool uApplyGamma;
+uniform float uGamma;
 
 void main()
 {
     oColor = vec4(texture(uTexture, fTexCoord).rgb * fScale, 1);
-
-    if (uApplyGamma)
-        oColor.rgb = pow(oColor.rgb, vec3(1.0 / 2.2));
+    oColor.rgb = pow(clamp(oColor.rgb, 0, 1), vec3(uGamma));
 }
