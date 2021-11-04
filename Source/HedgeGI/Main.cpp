c59fe4301719ed0102daf7e29f8325c5d72f9440
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-11-04 21:31:28 +0300

    Call Eigen initParallel function.

--- Source/HedgeGI/Main.cpp
+++ Source/HedgeGI/Main.cpp
@@ -10,6 +10,8 @@ int32_t main(int32_t argc, const char* argv[])
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
     _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 
+    Eigen::initParallel();
+
     DirectX::Initialize();
     CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
