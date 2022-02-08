5a9cd52c1aeb360595db659ddc4020dc2d9c4654
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2022-02-08 18:13:06 +0300

    Update ImGui to 1.87.

--- Source/HedgeGI/ImGuiManager.cpp
+++ Source/HedgeGI/ImGuiManager.cpp
@@ -49,7 +49,7 @@ void ImGuiManager::initializeStyle()
     ImVec4* colors = style->Colors;
     colors[ImGuiCol_Text] = ImVec4(0.88f, 0.88f, 0.88f, 1.00f);
     colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-    colors[ImGuiCol_WindowBg] = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
+    colors[ImGuiCol_WindowBg] = ImVec4(0.196f, 0.196f, 0.196f, 1.00f);
     colors[ImGuiCol_ChildBg] = ImVec4(0.196f, 0.196f, 0.196f, 1.00f);
     colors[ImGuiCol_PopupBg] = ImVec4(0.13f, 0.13f, 0.13f, 1.00f);
     colors[ImGuiCol_Border] = ImVec4(0.13f, 0.13f, 0.13f, 1.00f);
